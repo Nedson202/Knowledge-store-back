12d9e47c7b64edd050dfdccbc9086fdd99ec5a45
Nedson202 <simeonsamson4@gmail..com>
2019-11-13 12:43:43 +0100

    dev-google-auth-update
    * Increase google auth scope
    * Upgrade passport google auth to 2.0

--- passport/index.js
+++ passport/index.js
@@ -8,6 +8,7 @@ const { checkUserExists } = UserController;
 
 const saveUser = async (profile, done) => {
   const currentUser = await checkUserExists(profile.id);
+
   if (currentUser.username) {
     const payload = {
       id: currentUser.id,
@@ -19,17 +20,20 @@ const saveUser = async (profile, done) => {
     };
     const token = Utils.generateToken(payload);
     payload.token = token;
+
     return done(null, payload);
   }
+
   const newUser = {
     username: profile.displayName,
-    email: profile.emails[0].value,
+    email: profile.emails && profile.emails[0].value,
     password: process.env.FAKE_PASSWORD,
     socialId: profile.id,
-    picture: profile.photos[0].value,
+    picture: profile.photos && profile.photos[0].value,
     isVerified: 'true',
     socialAuth: true,
   };
+
   UserController.addUser(newUser).then((user) => {
     done(null, user);
   });
@@ -46,7 +50,6 @@ passport.use(new FacebookStrategy({
 
 passport.use(
   new GoogleStrategy({
-    // options for google strategy
     callbackURL: process.env.GOOGLE_CALLBACKURL,
     clientID: process.env.GOOGLE_CLIENTID,
     clientSecret: process.env.GOOGLE_CLIENTSECRET,
--- routes/index.js
+++ routes/index.js
@@ -6,33 +6,54 @@ const router = express.Router();
 const redirectUrl = process.env.NODE_ENV.match('production')
   ? process.env.PROD_SERVER : 'http://localhost:3000';
 
+let mobileRedirectUrl;
+
 // auth with google
-router.get('/google', passport.authenticate('google', {
-  scope: ['email', 'profile']
-}));
+router.get('/google', (req, res, next) => {
+  const { redirect_url: customRedirectURL } = req.query;
+
+  mobileRedirectUrl = customRedirectURL;
+
+  passport.authenticate('google', {
+    scope: [
+      'https://www.googleapis.com/auth/plus.login',
+      'https://www.googleapis.com/auth/plus.profile.emails.read',
+      'email',
+      'profile'
+    ]
+  })(req, res, next);
+});
 
 // callback route for google to redirect to
-router.get('/google/redirect',
-  passport.authenticate('google',
-    { session: false, failureRedirect: '/login' }),
-  (req, res) => {
-    const { token } = req.user;
-    return res.redirect(`${redirectUrl}/?token=${token}`);
-  });
+router.get('/google/redirect', passport.authenticate('google', {
+  session: false,
+  failureRedirect: '/login'
+}), (req, res) => {
+  let buildRedirectURL = redirectUrl;
+  const { token } = req.user;
+
+  if (mobileRedirectUrl) {
+    buildRedirectURL = mobileRedirectUrl;
+  }
+
+  return res.redirect(`${buildRedirectURL}?token=${token}`);
+});
 
 // auth with facebook
 router.get('/facebook', passport.authenticate('facebook', {
   scope: 'email'
 }));
 
-router.get('/facebook/redirect',
-  passport.authenticate('facebook',
-    { session: false, failureRedirect: '/login' }),
-  (req, res) => {
-    res.send(req.user);
-  });
+router.get('/facebook/redirect', passport.authenticate('facebook', {
+  session: false,
+  failureRedirect: '/login'
+}), (req, res) => {
+  const { token } = req.user;
 
-router.get('/healthChecker', (req, res) => res.status(200).json({
+  return res.redirect(`${redirectUrl}?token=${token}`);
+});
+
+router.get('/health', (req, res) => res.status(200).json({
   error: false,
   message: 'Health check completed, server is online'
 }));
